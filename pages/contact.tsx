ce27cc6c269c1ad7f7ea0918b4c30a6cf0c8c7af
Yanis Harrat <110360140+yanix2445@users.noreply.github.com>
2023-01-17 01:10:57 +0100

    Update contact.tsx

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -33,21 +33,18 @@ const contact = () => {
               <div className="col-1">
                 <div className="right">
 
-                  <form name="contact2.0" method="POST" data-netlify="true">
+                  <form name="contact2.0" method="POST" >
                     <p>
-                      <label>Your Name: <input type="text" name="name" /></label>
+                      <label for="name"> Your Name: </label>
+                      <input type="text" name="name" />
                     </p>
                     <p>
-                      <label>Your Email: <input type="email" name="email" /></label>
+                      <label for="email"> Your Email: </label>
+                       <input type="email" name="email" />
                     </p>
-                    {/* <p>
-                      <label>Your Role: <select name="role[]" multiple>
-                        <option value="leader">Leader</option>
-                        <option value="follower">Follower</option>
-                      </select></label>
-                    </p> */}
                     <p>
-                      <label>Message: <textarea name="message"></textarea></label>
+                      <labe for="email"> Message: </label>
+                      <textarea name="message"></textarea>
                     </p>
                     <p>
                       <button type="submit">Send</button>
